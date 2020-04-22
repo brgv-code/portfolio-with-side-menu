cd893866a690c04ae0b59d40b49aa1cfcd4f0a59
Bhargav Virupaakshi <brgv95@gmail.com>
2020-04-22 17:50:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+$(document).ready(function(e){
+  $win = $(window);
+  $navbar  = $('#header');
+  $toggle = $('.toggle-button');
+  var width = $navbar.width();
+  toggle_onclick($win,$navbar,width);
+
+    //resize event when the window resize happens , the transition should be responsive
+    $win.resize(function(){
+        toggle_onclick($win,$navbar,width);
+    });
+    $toggle.click(function(e){
+        $navbar.toggleClass("toggle-left");
+    })
+
+});
+
+function toggle_onclick($win,$navbar,width){
+    if( $win.width() <= 768 ){
+    $navbar.css({left:"-200px"});
+       // $navbar.css({left:`-$(width)px`});
+}else{
+    $navbar.css({left:"0px"});
+}
+
+}
+
+
+
+var typed = new Typed('#typed',{
+    strings:[
+        'Web Developer',
+        'Photographer'
+    ],
+    typeSpeed: 50,
+    backSpeed:50,
+    loop:true
+});
+
+
+var typed = new Typed('#typed_2',{
+    strings:[
+        'Web Developer',
+        'Photographer'
+    ],
+    typeSpeed: 50,
+    backSpeed:50,
+    loop:true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click',function(e){
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
